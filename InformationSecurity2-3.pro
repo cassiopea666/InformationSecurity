bfc52081ab39b278ce06f98de129bb1c44ac788c
Mariia Chernetska <mary.cherrnetska@gmail.com>
2015-05-27 17:25:59 +0300

    Added resource files

--- InformationSecurity2-3.pro
+++ InformationSecurity2-3.pro
@@ -18,3 +18,6 @@ SOURCES += main.cpp\
 HEADERS  += menu.h
 
 FORMS    += menu.ui
+
+RESOURCES += \
+    resource.qrc
---
+++ build-InformationSecurity2-3-Desktop_Qt_5_4_1_MSVC2013_OpenGL_64bit-Debug/ui_menu.h
@@ -0,0 +1,105 @@
+/********************************************************************************
+** Form generated from reading UI file 'menu.ui'
+**
+** Created by: Qt User Interface Compiler version 5.4.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MENU_H
+#define UI_MENU_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Menu
+{
+public:
+    QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QWidget *tab_2;
+    QFrame *frame;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *Menu)
+    {
+        if (Menu->objectName().isEmpty())
+            Menu->setObjectName(QStringLiteral("Menu"));
+        Menu->resize(898, 625);
+        centralWidget = new QWidget(Menu);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QStringLiteral("tabWidget"));
+        tab = new QWidget();
+        tab->setObjectName(QStringLiteral("tab"));
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QStringLiteral("tab_2"));
+        tabWidget->addTab(tab_2, QString());
+
+        gridLayout->addWidget(tabWidget, 1, 0, 1, 1);
+
+        frame = new QFrame(centralWidget);
+        frame->setObjectName(QStringLiteral("frame"));
+        frame->setMinimumSize(QSize(0, 200));
+        frame->setAutoFillBackground(false);
+        frame->setFrameShape(QFrame::Panel);
+        frame->setFrameShadow(QFrame::Raised);
+        frame->setLineWidth(3);
+
+        gridLayout->addWidget(frame, 0, 0, 1, 1);
+
+        Menu->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(Menu);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 898, 21));
+        Menu->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(Menu);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        Menu->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(Menu);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        Menu->setStatusBar(statusBar);
+
+        retranslateUi(Menu);
+
+        QMetaObject::connectSlotsByName(Menu);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *Menu)
+    {
+        Menu->setWindowTitle(QApplication::translate("Menu", "Menu", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("Menu", "Tab 1", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("Menu", "Tab 2", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Menu: public Ui_Menu {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MENU_H
