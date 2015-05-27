6ead3d981103ed959e2079e06b58cfef90983a60
Mariia Chernetska <mary.cherrnetska@gmail.com>
2015-05-27 18:10:37 +0300

    Created Form

--- build-InformationSecurity2-3-Desktop_Qt_5_4_1_MSVC2013_OpenGL_64bit-Debug/ui_menu.h
+++ build-InformationSecurity2-3-Desktop_Qt_5_4_1_MSVC2013_OpenGL_64bit-Debug/ui_menu.h
@@ -15,12 +15,16 @@
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QFrame>
 #include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTabWidget>
 #include <QtWidgets/QToolBar>
+#include <QtWidgets/QToolButton>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -31,9 +35,22 @@ public:
     QWidget *centralWidget;
     QGridLayout *gridLayout;
     QTabWidget *tabWidget;
-    QWidget *tab;
-    QWidget *tab_2;
+    QWidget *tab_ciphers;
+    QWidget *tab_diffiehellman;
+    QWidget *tab_symmetric;
+    QWidget *tab_hash;
+    QWidget *tab_rsa;
     QFrame *frame;
+    QWidget *widget;
+    QGridLayout *gridLayout_2;
+    QLineEdit *lineEdit;
+    QToolButton *toolButton_2;
+    QLineEdit *lineEdit_2;
+    QToolButton *toolButton;
+    QWidget *widget1;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLabel *label_3;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -51,12 +68,21 @@ public:
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
         tabWidget = new QTabWidget(centralWidget);
         tabWidget->setObjectName(QStringLiteral("tabWidget"));
-        tab = new QWidget();
-        tab->setObjectName(QStringLiteral("tab"));
-        tabWidget->addTab(tab, QString());
-        tab_2 = new QWidget();
-        tab_2->setObjectName(QStringLiteral("tab_2"));
-        tabWidget->addTab(tab_2, QString());
+        tab_ciphers = new QWidget();
+        tab_ciphers->setObjectName(QStringLiteral("tab_ciphers"));
+        tabWidget->addTab(tab_ciphers, QString());
+        tab_diffiehellman = new QWidget();
+        tab_diffiehellman->setObjectName(QStringLiteral("tab_diffiehellman"));
+        tabWidget->addTab(tab_diffiehellman, QString());
+        tab_symmetric = new QWidget();
+        tab_symmetric->setObjectName(QStringLiteral("tab_symmetric"));
+        tabWidget->addTab(tab_symmetric, QString());
+        tab_hash = new QWidget();
+        tab_hash->setObjectName(QStringLiteral("tab_hash"));
+        tabWidget->addTab(tab_hash, QString());
+        tab_rsa = new QWidget();
+        tab_rsa->setObjectName(QStringLiteral("tab_rsa"));
+        tabWidget->addTab(tab_rsa, QString());
 
         gridLayout->addWidget(tabWidget, 1, 0, 1, 1);
 
@@ -67,6 +93,87 @@ public:
         frame->setFrameShape(QFrame::Panel);
         frame->setFrameShadow(QFrame::Raised);
         frame->setLineWidth(3);
+        widget = new QWidget(frame);
+        widget->setObjectName(QStringLiteral("widget"));
+        widget->setGeometry(QRect(10, 30, 861, 27));
+        gridLayout_2 = new QGridLayout(widget);
+        gridLayout_2->setSpacing(6);
+        gridLayout_2->setContentsMargins(11, 11, 11, 11);
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        gridLayout_2->setContentsMargins(0, 0, 0, 0);
+        lineEdit = new QLineEdit(widget);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setEnabled(false);
+        lineEdit->setMinimumSize(QSize(0, 25));
+        QPalette palette;
+        QBrush brush(QColor(77, 77, 77, 255));
+        brush.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Shadow, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Shadow, brush);
+        palette.setBrush(QPalette::Disabled, QPalette::Shadow, brush);
+        lineEdit->setPalette(palette);
+
+        gridLayout_2->addWidget(lineEdit, 0, 0, 1, 1);
+
+        toolButton_2 = new QToolButton(widget);
+        toolButton_2->setObjectName(QStringLiteral("toolButton_2"));
+        toolButton_2->setMinimumSize(QSize(25, 25));
+        QIcon icon;
+        icon.addFile(QStringLiteral(":/ico/folder_add_24.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        toolButton_2->setIcon(icon);
+        toolButton_2->setIconSize(QSize(20, 20));
+        toolButton_2->setPopupMode(QToolButton::DelayedPopup);
+        toolButton_2->setToolButtonStyle(Qt::ToolButtonIconOnly);
+        toolButton_2->setAutoRaise(false);
+
+        gridLayout_2->addWidget(toolButton_2, 0, 1, 1, 1);
+
+        lineEdit_2 = new QLineEdit(widget);
+        lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
+        lineEdit_2->setEnabled(false);
+        lineEdit_2->setMinimumSize(QSize(0, 25));
+
+        gridLayout_2->addWidget(lineEdit_2, 0, 2, 1, 1);
+
+        toolButton = new QToolButton(widget);
+        toolButton->setObjectName(QStringLiteral("toolButton"));
+        toolButton->setMinimumSize(QSize(25, 25));
+        toolButton->setIcon(icon);
+        toolButton->setIconSize(QSize(20, 20));
+
+        gridLayout_2->addWidget(toolButton, 0, 3, 1, 1);
+
+        widget1 = new QWidget(frame);
+        widget1->setObjectName(QStringLiteral("widget1"));
+        widget1->setGeometry(QRect(10, 10, 861, 21));
+        horizontalLayout = new QHBoxLayout(widget1);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(widget1);
+        label->setObjectName(QStringLiteral("label"));
+        label->setFrameShape(QFrame::NoFrame);
+        label->setWordWrap(false);
+        label->setMargin(1);
+
+        horizontalLayout->addWidget(label);
+
+        label_3 = new QLabel(widget1);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setFrameShape(QFrame::NoFrame);
+        label_3->setWordWrap(false);
+        label_3->setMargin(1);
+
+        horizontalLayout->addWidget(label_3);
+
+        tabWidget->raise();
+        lineEdit_2->raise();
+        lineEdit->raise();
+        toolButton_2->raise();
+        toolButton->raise();
+        label->raise();
+        label_3->raise();
 
         gridLayout->addWidget(frame, 0, 0, 1, 1);
 
@@ -84,14 +191,24 @@ public:
 
         retranslateUi(Menu);
 
+        tabWidget->setCurrentIndex(0);
+
+
         QMetaObject::connectSlotsByName(Menu);
     } // setupUi
 
     void retranslateUi(QMainWindow *Menu)
     {
         Menu->setWindowTitle(QApplication::translate("Menu", "Menu", 0));
-        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("Menu", "Tab 1", 0));
-        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("Menu", "Tab 2", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_ciphers), QApplication::translate("Menu", "\342\204\226  1", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_diffiehellman), QApplication::translate("Menu", "\342\204\226  2", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_symmetric), QApplication::translate("Menu", "\342\204\226  3", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_hash), QApplication::translate("Menu", "\342\204\226  4", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_rsa), QApplication::translate("Menu", "\342\204\226  5", 0));
+        toolButton_2->setText(QApplication::translate("Menu", "...", 0));
+        toolButton->setText(QApplication::translate("Menu", "...", 0));
+        label->setText(QApplication::translate("Menu", "\320\250\320\273\321\217\321\205 \320\264\320\276 \321\204\320\260\320\271\320\273\321\203 \320\267 \320\262\321\226\320\264\320\272\321\200\320\270\321\202\320\270\320\274 \321\202\320\265\320\272\321\201\321\202\320\276\320\274 (\320\264\320\273\321\217 \321\210\320\270\321\204\321\200\321\203\320\262\320\260\320\275\320\275\321\217):", 0));
+        label_3->setText(QApplication::translate("Menu", "\320\250\320\273\321\217\321\205 \320\264\320\276 \321\204\320\260\320\271\320\273\321\203 \320\267 \321\210\320\270\321\204\321\200\320\276\321\202\320\265\320\272\321\201\321\202\320\276\320\274 (\320\264\320\273\321\217 \320\264\320\265\321\210\320\270\321\204\321\200\321\203\320\262\320\260\320\275\320\275\321\217):", 0));
     } // retranslateUi
 
 };
